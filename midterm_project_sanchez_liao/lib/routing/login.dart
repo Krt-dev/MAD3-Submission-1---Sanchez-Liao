2cfe2502fc4c032c806a4cae71a8fc61c7d90a7e
Kurt Sanchez <kurt.sanchez.20@usjr.edu.ph>
2024-06-22 16:36:03 +0800

    added validation of inputs

--- midterm_project_sanchez_liao/lib/routing/login.dart
+++ midterm_project_sanchez_liao/lib/routing/login.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 // import 'package:go_router/go_router.dart';
 import 'package:midterm_project_sanchez_liao/router.dart';
 import 'package:midterm_project_sanchez_liao/routing/homeScreen.dart';
+import 'package:form_field_validator/form_field_validator.dart';
 
 class LoginScreen extends StatefulWidget {
   static const String route = "/auth";
@@ -15,6 +16,25 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  final emailValidator = MultiValidator([
+    RequiredValidator(errorText: 'Email is required'),
+    EmailValidator(errorText: 'Enter a valid email address'),
+    MaxLengthValidator(32, errorText: "Username cannot exceed 32 characters"),
+    PatternValidator(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$',
+        errorText: 'Username cannot contain special characters'),
+  ]);
+
+  final passwordValidator = MultiValidator([
+    RequiredValidator(errorText: 'Password is required'),
+    MinLengthValidator(8,
+        errorText: 'Password must be at least 8 characters long'),
+    PatternValidator(r'(?=.*[A-Z])',
+        errorText: 'Password must contain at least one uppercase letter'),
+    PatternValidator(r'(?=.*?[#?!@$%^&*-])',
+        errorText: 'Password must contain at least one special character'),
+  ]);
 
   @override
   Widget build(BuildContext context) {
@@ -24,29 +44,43 @@ class _LoginScreenState extends State<LoginScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(
-              controller: _usernameController,
-              decoration: const InputDecoration(
-                labelText: 'Username',
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextFormField(
+                controller: _usernameController,
+                decoration: const InputDecoration(
+                  labelText: 'Username',
+                ),
+                validator: emailValidator.call,
               ),
-            ),
-            TextField(
-              controller: _passwordController,
-              decoration: const InputDecoration(
-                labelText: 'Password',
+              TextFormField(
+                controller: _passwordController,
+                decoration: const InputDecoration(
+                  labelText: 'Password',
+                ),
+                obscureText: true,
+                validator: passwordValidator.call,
               ),
-              obscureText: true,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                GlobalRouter.I.router.go(HomeScreen.route);
-              },
-              child: const Text("Login"),
-            ),
-          ],
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState?.validate() ?? false) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text('Logged in',
+                            style: TextStyle(color: Colors.white)),
+                        backgroundColor: Colors.green,
+                      ),
+                    );
+                    GlobalRouter.I.router.go(HomeScreen.route);
+                  }
+                },
+                child: const Text("Login"),
+              ),
+            ],
+          ),
         ),
       ),
     );
